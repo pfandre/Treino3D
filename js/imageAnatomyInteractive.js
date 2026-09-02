4134b015dc3948baa6ea260328a53cceb343b260
André Peixoto <aaddreee@gmail.com>
2026-09-02 15:36:42 -0300

    fix: deslocar ponto dos femorais 4cm para o centro (apenas os pontos)

--- js/imageAnatomyInteractive.js
+++ js/imageAnatomyInteractive.js
@@ -195,8 +195,8 @@ export class ImageAnatomyInteractive {
         <path data-muscle="antebrazos" data-pin-x="65" data-pin-y="43" class="anatomy-trigger-area" d="M 655 360 C 675 360, 686 375, 686 395 L 660 495 C 640 495, 619 480, 619 455 L 655 360 Z" />
         <path data-muscle="gluteos" data-pin-x="46" data-pin-y="51" class="anatomy-trigger-area" d="M 355 430 C 420 425, 498 440, 498 480 C 495 535, 440 548, 415 542 C 390 515, 352 478, 355 430 Z" />
         <path data-muscle="gluteos" data-pin-x="54" data-pin-y="51" class="anatomy-trigger-area" d="M 645 430 C 580 425, 502 440, 502 480 C 505 535, 560 548, 585 542 C 610 515, 648 478, 645 430 Z" />
-        <path data-muscle="femorales" data-pin-x="38" data-pin-y="64" class="anatomy-trigger-area" d="M 345 550 C 400 545, 490 550, 490 558 L 478 755 C 420 752, 345 730, 345 695 Z" />
-        <path data-muscle="femorales" data-pin-x="62" data-pin-y="64" class="anatomy-trigger-area" d="M 510 558 C 510 550, 600 545, 655 550 L 655 695 C 655 730, 580 752, 522 755 Z" />
+        <path data-muscle="femorales" data-pin-x="44" data-pin-y="64" class="anatomy-trigger-area" d="M 345 550 C 400 545, 490 550, 490 558 L 478 755 C 420 752, 345 730, 345 695 Z" />
+        <path data-muscle="femorales" data-pin-x="56" data-pin-y="64" class="anatomy-trigger-area" d="M 510 558 C 510 550, 600 545, 655 550 L 655 695 C 655 730, 580 752, 522 755 Z" />
         <path data-muscle="pantorrillas" data-pin-x="38" data-pin-y="82" class="anatomy-trigger-area" d="M 335 760 C 385 755, 475 760, 475 768 L 460 920 C 405 925, 335 908, 335 888 Z" />
         <path data-muscle="pantorrillas" data-pin-x="62" data-pin-y="82" class="anatomy-trigger-area" d="M 525 768 C 525 760, 615 755, 665 760 L 665 888 C 665 908, 595 925, 540 920 Z" />
       `,
