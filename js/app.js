d781c5dedd4d544e997c4b8c3a789ef2d182599f
André Peixoto <aaddreee@gmail.com>
2026-09-02 16:56:57 -0300

    chore: bump cache to v20 for hotfix

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
  * App.js - Orquestrador Principal do Aplicativo Anatômico Profissional Mestre
  */
 
-import { MUSCLE_DATABASE } from './database.js?v=19';
-import { ImageAnatomyInteractive } from './imageAnatomyInteractive.js?v=19';
-import { OrganicAnatomySVG } from './organicAnatomySVG.js?v=19';
-import { ExerciseUI } from './exerciseUI.js?v=19';
-import { EditorModal } from './editorModal.js?v=19';
-import { WorkoutPlanner } from './workoutPlanner.js?v=19';
-import { SoundEffects } from './soundEffects.js?v=19';
+import { MUSCLE_DATABASE } from './database.js?v=20';
+import { ImageAnatomyInteractive } from './imageAnatomyInteractive.js?v=20';
+import { OrganicAnatomySVG } from './organicAnatomySVG.js?v=20';
+import { ExerciseUI } from './exerciseUI.js?v=20';
+import { EditorModal } from './editorModal.js?v=20';
+import { WorkoutPlanner } from './workoutPlanner.js?v=20';
+import { SoundEffects } from './soundEffects.js?v=20';
 
 document.addEventListener('DOMContentLoaded', () => {
   EditorModal.loadPersistedData();
